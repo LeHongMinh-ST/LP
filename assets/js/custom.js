53aefec43a8392527eef51724017e883fccc8351
minhlh <hongminhle290@gmail.com>
2023-12-17 00:24:55 +0700

    init

---
+++ assets/js/custom.js
@@ -0,0 +1,37 @@
+(function ($) {
+    'use strict';
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 150) {
+            $('.navbar-area').addClass("is-sticky");
+        } else {
+            $('.navbar-area').removeClass("is-sticky");
+        }
+
+    });
+
+
+
+})(jQuery);
+
+const swiper = new Swiper('#mySwiper', {
+  // Optional parameters
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    enabled: false,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    enabled: false,
+  },
+});
